061220bc16c067e3489e4e68d4c7e5822463c80d
Gabriel Gregório <gabrielgregorio@pop-os.localdomain>
2022-08-09 15:34:25 -0300

    feat: cria a classe abstrata Race

---
+++ src/Races/Race.ts
@@ -0,0 +1,21 @@
+abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+
+  get name() { return this._name; }
+
+  get dexterity() { return this._dexterity; }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
+
+export default Race;
