cfc757c6168c2bf83042c9c3feebb3fa77cae924
Gabriel Gregório <gabrielgregorio@pop-os.localdomain>
2022-08-11 15:03:34 -0300

    feat: cria a classe Monster

---
+++ src/Monster.ts
@@ -0,0 +1,25 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints(): number { return this._lifePoints; }
+  get strength(): number { return this._strength; }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = attackPoints - this._lifePoints;
+    if (damage > 0) return this._lifePoints - damage;
+    if (this._lifePoints < 0) return -1;
+    return this._lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+}
